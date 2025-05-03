d4982e8f37f3a76660ca3f8ef6cbd232eeb28d5e
AlexSuper21 <topprodaz@gmail.com>
2025-05-03 14:52:54 +0300

    Add files via upload

---
+++ jsscript.js
@@ -0,0 +1,7 @@
+// Открытие формы заявки
+document.querySelector('.btn-call').addEventListener('click', function() {
+    alert('Форма заказа звонка будет здесь!');
+});
+
+// Можно добавить слайдер для проектов
+// Например, с использованием библиотеки Swiper.js
