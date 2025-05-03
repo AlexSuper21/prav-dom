b25379dd202cb5aa53c522a558c707b4eb3615e7
AlexSuper21 <topprodaz@gmail.com>
2025-05-03 16:27:21 +0300

    Delete jsscript.js

--- jsscript.js
+++
@@ -1,9 +0,0 @@
-// Загрузка конфига
-fetch('config.json')
-    .then(response => response.json())
-    .then(config => {
-        // Динамическое заполнение hero-секции
-        document.querySelector('.hero h1').textContent = config.hero.title;
-        document.querySelector('.hero p').textContent = config.hero.description;
-        // ...и т.д.
-    });
